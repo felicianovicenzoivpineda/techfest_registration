41633f731b596e34d32a47214c322b052bc7f7e9
Feliciano Vicenzo Pineda Iv <12410903@benilde.edu.ph>
2025-11-06 08:35:01 +0800

    Implemented registration setup

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,1 +1,10 @@
-print("Hello World")
+print("Welcome to SMIT TechFest!")
+print("Event organized by Feliciano Vicenzo Pineda IV of APPDAET BTCS1")
+
+parti = input(int("How many participants will register?"))
+while True:
+    if parti < 0:
+        print("Invaild number of participants.")
+        break
+    else:
+        continue
