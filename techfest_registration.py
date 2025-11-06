400d54027b51c7d27270f26627fc8c243ced4761
Feliciano Vicenzo Pineda Iv <12410903@benilde.edu.ph>
2025-11-06 08:43:37 +0800

    Added project README

--- techfest_registration.py
+++ techfest_registration.py
@@ -1,10 +1,23 @@
+parti_name = []
+parti_course = []
 print("Welcome to SMIT TechFest!")
 print("Event organized by Feliciano Vicenzo Pineda IV of APPDAET BTCS1")
 
-parti = input(int("How many participants will register?"))
+num_parti = input(int("How many participants will register?"))
 while True:
-    if parti < 0:
+    if num_parti < 0:
         print("Invaild number of participants.")
         break
     else:
         continue
+for i in range(len(num_parti)):
+    parti_name.append(input("\nEnter participant's name: "))
+    parti_course.append(input("\nEnter chosen track: "))
+
+participant = {"parti": parti_name, "course": parti_course}
+
+print("\nRegistered Participants:")
+index = 1
+while index < len(parti_name):
+    print(participant)
+    index += 1
